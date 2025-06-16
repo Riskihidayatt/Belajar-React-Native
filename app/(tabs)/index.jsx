477ff593ab574c3ee58cf8f5968feb3c30e3e118
Riskihidayatt <riski191203@gmail.com>
2025-06-16 13:26:40 +0700

    Belajar React Native images

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,29 @@
+import { View,  StyleSheet } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+const PlaceholderImage = require('@/assets/images/oke.jpg');
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
